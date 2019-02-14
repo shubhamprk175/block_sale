25e47d2a03d15e6a0e83c19345ba78b3ced4a4fc
Abhav <abhavluthra18@gmail.com>
2019-02-14 14:30:15 -0500

    view updated

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('<int:user_id>', views.user, name='user'),
+    path("register/", views.register, name = "register"),
+    path("logout/", views.logout_request, name = "logout"),
+	path("login/", views.login_request, name="login")
 ]
