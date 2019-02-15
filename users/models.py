27df9c1db33281110327659c2298edfe65c453ed
Abhav <abhavluthra18@gmail.com>
2019-02-15 18:27:19 -0500

    user file reverted

--- users/models.py
+++ users/models.py
@@ -2,11 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
-	user_name = models.CharField(max_length=50) #, default = 'user name')
-	first_name = models.CharField(max_length=50)#, default = 'first name')
-	last_name = models.CharField(max_length=50)#, default = 'last name')
-	password = models.CharField(max_length=256)#, default='password')
+class User(models.Model):
+	user_name = models.CharField(max_length=50)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
 	email = models.EmailField(blank=True, unique=True)
 	balance = models.DecimalField(decimal_places=2, max_digits=20, default=100)
 	image = models.ImageField(upload_to='photos/%Y/%m/%d', null=True, blank=True)
