96c305b41f65a01b038bbbca394d9d5a0c44e1a5
Abhav <abhavluthra18@gmail.com>
2019-03-17 19:16:54 -0400

    Contracts updated with modifiers

--- BlockSale/contracts/BlockSale.sol
+++ BlockSale/contracts/BlockSale.sol
@@ -5,19 +5,19 @@ import "./Owner.sol";
 contract BlockSale is Owner{
 
     bytes32 public transactionHash;
+
     event SaleComplete(address from, address to);
 
+    // Buy Transaction is Recorded
     function buy(bytes memory productString) public{
          transactionHash = keccak256(productString);
     }
 
-    
+    // Settle Payment between Buyer and Seller
     function settlePayment(address buyer,address seller, uint amount) public{
-        //subtract money
-        if (balances[buyer] < amount) return;
+        if (balances[buyer] < amount) {revert();}
         balances[buyer] -= amount;
         balances[seller] += amount;
         emit SaleComplete(buyer, seller);
-        
     }
 }
