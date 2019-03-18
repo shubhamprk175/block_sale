29d541a4ea1982905adb45ace9ebba2890db669a
Abhav <abhavluthra18@gmail.com>
2019-03-17 22:05:55 -0400

    Add Product Changes

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from .forms import ProductForm
 from .models import Product
 
 # Create your views here.
@@ -16,3 +16,13 @@ def product(request, product_id):
         'product': product
     }
     return render(request, 'products/product.html', context)
+
+def addproduct(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product_item = form.save(commit = False)
+            product_item.save()
+    else:
+        form = ProductForm()
+    return render(request, 'products/addproduct.html', {"form":form})
