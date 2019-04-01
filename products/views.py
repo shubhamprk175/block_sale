3adc97345ee725ac34456e5cec2041cb233ef794
Abhav <abhavluthra18@gmail.com>
2019-04-01 16:45:39 -0400

    Add product

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import ProductForm
 from .models import Product
+import datetime
 
 # Create your views here.
 def products(request):
@@ -19,10 +20,16 @@ def product(request, product_id):
 
 def addproduct(request):
     if request.method == "POST":
-        form = ProductForm(request.POST)
+        print(request.POST['image'])
+        form = ProductForm(request.POST, request.FILES)
+        # form['image'] = request.POST['image']
         if form.is_valid():
-            product_item = form.save(commit = False)
+            product_item = form.save(commit=False)
+            d = datetime.date.today()
+            directory = f"photos/{d.year}/{d:%m}/{d:%d}/"
+            product_item.image = directory + request.POST['image']
             product_item.save()
+            print(form.cleaned_data)
     else:
         form = ProductForm()
     return render(request, 'products/addproduct.html', {"form":form})
